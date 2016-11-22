771f2e8974540c535f7785747430c1faf3547be3
ShaneQi <qizengtai@gmail.com>
2016-11-22 02:33:03 -0500

    Added Realm connecting in launching.

---
+++ app/src/main/java/com/github/jolinzhang/petcare/ThisApplication.java
@@ -0,0 +1,42 @@
+package com.github.jolinzhang.petcare;
+
+/**
+ * Created by Shadow on 11/21/16.
+ */
+
+import android.app.Application;
+import android.widget.Toast;
+
+import com.github.jolinzhang.model.Event;
+import com.github.jolinzhang.model.Pet;
+
+import io.realm.ObjectServerError;
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.SyncConfiguration;
+import io.realm.SyncCredentials;
+import io.realm.SyncUser;
+
+public class ThisApplication extends Application implements SyncUser.Callback {
+
+    public static final String AUTH_URL = "http://" + "162.243.66.53" + ":9080/auth";
+    public static final String REALM_URL = "realm://" + "162.243.66.53" + ":9080/~/petcare";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+        SyncUser.loginAsync(SyncCredentials.usernamePassword("diva@petcare.com", "can", false), ThisApplication.AUTH_URL, this);
+    }
+
+    @Override
+    public void onSuccess(SyncUser user) {
+        SyncConfiguration defaultConfig = new SyncConfiguration.Builder(user, ThisApplication.REALM_URL).build();
+        Realm.setDefaultConfiguration(defaultConfig);
+    }
+
+    @Override
+    public void onError(ObjectServerError error) {
+        Toast.makeText(this, "Failed connecting to database.", Toast.LENGTH_LONG).show();
+    }
+}
