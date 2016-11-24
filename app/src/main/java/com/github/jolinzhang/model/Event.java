1726b72265f993c48b8c6034c69c49f9c3055599
ShaneQi <qizengtai@gmail.com>
2016-11-23 19:48:43 -0500

    Added models.

---
+++ app/src/main/java/com/github/jolinzhang/model/Event.java
@@ -0,0 +1,96 @@
+package com.github.jolinzhang.model;
+
+import java.util.Date;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by Shadow on 11/21/16.
+ */
+
+public class Event extends RealmObject {
+    @PrimaryKey
+    private String id;
+
+    private String title;
+    private String description;
+    private byte[] picture;
+    private double longitude;
+    private double latitude;
+    private Date datetime;
+    private boolean isCompleted;
+    private Pet owner;
+
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public Pet getOwner() {
+        return owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setDatetime(Date datetime) {
+        this.datetime = datetime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setOwner(Pet owner) {
+        this.owner = owner;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+}
---
+++ app/src/main/java/com/github/jolinzhang/model/Pet.java
@@ -0,0 +1,118 @@
+package com.github.jolinzhang.model;
+
+import java.util.Date;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by Shadow on 11/21/16.
+ */
+
+public class Pet extends RealmObject {
+
+    @PrimaryKey
+    private String id;
+
+    private String name;
+    private String species;
+    private Date birthday;
+    private boolean isFemale;
+    private String vetName;
+    private String vetPhone;
+    private String chipId;
+    private String chipCompany;
+    private String medications;
+    private byte[] avatar;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public boolean isFemale() {
+        return isFemale;
+    }
+
+    public String getVetName() {
+        return vetName;
+    }
+
+    public String getVetPhone() {
+        return vetPhone;
+    }
+
+    public String getChipId() {
+        return chipId;
+    }
+
+    public String getChipCompany() {
+        return chipCompany;
+    }
+
+    public String getMedications() {
+        return medications;
+    }
+
+    public byte[] getAvatar() {
+        return avatar;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setFemale(boolean female) {
+        isFemale = female;
+    }
+
+    public void setVetName(String vetName) {
+        this.vetName = vetName;
+    }
+
+    public void setVetPhone(String vetPhone) {
+        this.vetPhone = vetPhone;
+    }
+
+    public void setChipId(String chipId) {
+        this.chipId = chipId;
+    }
+
+    public void setChipCompany(String chipCompany) {
+        this.chipCompany = chipCompany;
+    }
+
+    public void setMedications(String medications) {
+        this.medications = medications;
+    }
+
+    public void setAvatar(byte[] avatar) {
+        this.avatar = avatar;
+    }
+
+
+}
