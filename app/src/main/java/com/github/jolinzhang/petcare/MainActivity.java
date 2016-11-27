a2c05901ba7a6ef7b0b81cf48d1b3e590101094e
ShaneQi <qizengtai@gmail.com>
2016-11-27 14:54:39 -0600

    Added event activity.

--- app/src/main/java/com/github/jolinzhang/petcare/MainActivity.java
+++ app/src/main/java/com/github/jolinzhang/petcare/MainActivity.java
@@ -2,6 +2,7 @@ package com.github.jolinzhang.petcare;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
@@ -15,22 +16,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.github.jolinzhang.model.DataRepoConfig;
 import com.github.jolinzhang.model.DataRepository;
-import com.github.jolinzhang.model.IDataRepoConfig;
 import com.github.jolinzhang.model.Pet;
 import com.github.jolinzhang.petcare.Fragment.FutureEventFragment;
 import com.github.jolinzhang.petcare.Fragment.GalleryFragment;
 import com.github.jolinzhang.petcare.Fragment.SettingFragment;
 import com.github.jolinzhang.petcare.Fragment.TimeLineFragment;
 
-import io.realm.RealmChangeListener;
-import io.realm.RealmModel;
-import io.realm.RealmResults;
+import java.util.Date;
+
+import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,14 +36,6 @@ public class MainActivity extends AppCompatActivity
     private GalleryFragment galleryFragment;
     private SettingFragment settingFragment;
     private FragmentManager fragmentManager;
-    private FragmentTransaction transaction;
-
-    private  View header;
-    private Menu menuNav;
-    private ImageView mswitch;
-    private boolean  switchModel = false;
-    private RealmResults<Pet> pets;
-    private int  pet_id = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,18 +45,7 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         fragmentManager = getFragmentManager();
-        transaction = fragmentManager.beginTransaction();
-        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
 
-        //show TimeLineFragment as default fragment
-        timeLineFragment = (TimeLineFragment) fragmentManager.findFragmentByTag("TimeLineFragment");
-        if(timeLineFragment == null){
-            timeLineFragment = new TimeLineFragment();
-        }
-        transaction.replace(R.id.content_scrolling, timeLineFragment,"TimeLineFragment")
-                .commit();
-
-        //Floating Action Bar action
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,46 +61,8 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-
-        //switch button in nav header.
-        header = navigationView.getHeaderView(0);
-        mswitch = (ImageView) header.findViewById(R.id.switchButton);
-        mswitch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                menuNav = navigationView.getMenu();
-                if(switchModel == false){
-                    //menu_nav disable, show user_list
-                    menuNav.setGroupVisible(R.id.menu_nav, false);
-                    menuNav.setGroupVisible(R.id.user_list,true);
-                    pets = DataRepository.getInstance().getPets();
-
-                    pets.addChangeListener(new RealmChangeListener<RealmResults<Pet>>() {
-                        @Override
-                        public void onChange(RealmResults<Pet> element) {
-                            //reset all value after clicking every time
-                          menuNav.removeGroup(R.id.user_list);
-                            pet_id = 0;
-                          for(Pet pet: element){
-                                menuNav.add(R.id.user_list,pet_id++,Menu.NONE,pet.getName()).setIcon(R.drawable.ic_setting_account);
-                            }
-                            menuNav.add(R.id.user_list,pet_id++,Menu.NONE,"Add Pet").setIcon(R.drawable.ic_menu_add);
-                            switchModel = true;
-                            }
-                    });
-
-                }else {
-                    //show menu_nav, hide user_list
-                    menuNav.setGroupVisible(R.id.menu_nav, true);
-                    menuNav.setGroupVisible(R.id.user_list,false);
-                    switchModel = false;
-                }
-
-            }
-        });
 
     }
 
@@ -136,7 +75,6 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -160,36 +98,37 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-
-
-
-
-
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        transaction = fragmentManager.beginTransaction();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        
+
         if (id == R.id.timeLine) {
+//            Realm.getDefaultInstance().beginTransaction();
+//            Pet p = Realm.getDefaultInstance().where(Pet.class).equalTo("id", "hellodiva").findFirst();
+//            p.setBirthday(new Date());
+//            Realm.getDefaultInstance().copyToRealmOrUpdate(p);
+//            Realm.getDefaultInstance().commitTransaction();
+            Intent intent = new Intent(this, NewEventActivity.class);
+//            intent.putExtra("pet_id", "hellodiva");
+            startActivity(intent);
             //connect to timeLine Fragment
-            timeLineFragment = (TimeLineFragment) fragmentManager.findFragmentByTag("TimeLineFragment");
-            if(timeLineFragment == null){
-                timeLineFragment = new TimeLineFragment();
-            }
-            transaction.replace(R.id.content_scrolling, timeLineFragment,"TimeLineFragment")
-            .commit();
+//            timeLineFragment = (TimeLineFragment) fragmentManager.findFragmentByTag("TimeLineFragment");
+//            if(timeLineFragment == null){
+//                timeLineFragment = new TimeLineFragment();
+//            }
+//            transaction.replace(R.id.content_scrolling, timeLineFragment,"TimeLineFragment")
+//            .commit();
 
         } else if (id == R.id.featureEvent) {
             futureEventFragment = (FutureEventFragment) fragmentManager.findFragmentByTag("FutureEventFragment");
             if(futureEventFragment == null){
                 futureEventFragment = new FutureEventFragment();
             }
-
             transaction.replace(R.id.content_scrolling, futureEventFragment, "FutureEventFragment")
                     .commit();
         } else if (id == R.id.gallery) {
@@ -209,41 +148,8 @@ public class MainActivity extends AppCompatActivity
                     .commit();
         }
 
-
-        //handle change pet event here
-
-        if(pet_id > 0){
-            // add new pet
-            if(id == (pet_id-1)){
-
-
-
-
-
-            }
-            if(id< (pet_id-1)){
-                DataRepoConfig.getInstance().setCurrentPetId(pets.get(id).getId());
-
-                DataRepository.getInstance().getPet().addChangeListener(new RealmChangeListener<Pet>() {
-                    @Override
-                    public void onChange(Pet element) {
-                       TextView name = (TextView) header.findViewById(R.id.nav_pet_name);
-                        name.setText(element.getName());
-
-                    }
-                });
-
-                menuNav.setGroupVisible(R.id.menu_nav, true);
-                menuNav.setGroupVisible(R.id.user_list,false);
-                switchModel = false;
-                return true;
-            }
-        }
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-
         return true;
     }
 }
