13f5d6255279854942b02fac7e53c14911408944
ShaneQi <qizengtai@gmail.com>
2016-11-30 01:05:35 -0600

    Changed time line item style.

--- app/src/main/java/com/github/jolinzhang/petcare/Adapter/TimeLineAdapter.java
+++ app/src/main/java/com/github/jolinzhang/petcare/Adapter/TimeLineAdapter.java
@@ -8,15 +8,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.github.jolinzhang.model.DataRepoConfig;
 import com.github.jolinzhang.model.DataRepository;
 import com.github.jolinzhang.model.Event;
 import com.github.jolinzhang.petcare.EventActivity;
 import com.github.jolinzhang.petcare.R;
-
-import org.w3c.dom.Text;
+import com.github.jolinzhang.util.Util;
 
 import io.realm.RealmChangeListener;
 import io.realm.RealmResults;
@@ -34,11 +34,13 @@ public class TimeLineAdapter extends RecyclerView.Adapter<TimeLineAdapter.ViewHo
         private TextView title;
         private TextView content;
         private TextView time;
+        private ImageView pictureImageView;
         public ViewHolder(View view){
             super(view);
             title = (TextView) view.findViewById(R.id.title);
             content = (TextView) view.findViewById(R.id.content);
             time = (TextView) view.findViewById(R.id.time);
+            pictureImageView = (ImageView) view.findViewById(R.id.timeline_item_picture);
         }
     }
 
@@ -64,16 +66,34 @@ public class TimeLineAdapter extends RecyclerView.Adapter<TimeLineAdapter.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
-        holder.title.setText(events.get(position).getTitle());
-        holder.content.setText(events.get(position).getDescription());
-        holder.time.setText(events.get(position).getDatetime().toString());
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        final Event thisEvent = events.get(position);
+        if (thisEvent.hasPicture()) {
+            holder.pictureImageView.setVisibility(View.VISIBLE);
+            holder.pictureImageView.post(new Runnable() {
+                @Override
+                public void run() {
+                    int width = holder.pictureImageView.getMeasuredWidth();
+                    int height = width;
+                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, height);
+                    holder.pictureImageView.setLayoutParams(layoutParams);
+                    Util.getInstance().loadImage(thisEvent.getId(), holder.pictureImageView, false);
+                }
+            });
+        } else {
+            holder.pictureImageView.setVisibility(View.GONE);
+        }
+
+
+        holder.title.setText(thisEvent.getTitle());
+        holder.content.setText(thisEvent.getDescription());
+        holder.time.setText(Util.getInstance().dateFormatter().format(thisEvent.getDatetime()));
         //item click issue
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(context, EventActivity.class);
-                String event_id = events.get(position).getId();
+                String event_id = thisEvent.getId();
                 intent.putExtra("event_id",event_id);
                 context.startActivity(intent);
             }
